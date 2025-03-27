a856bcbd4fa7e9b0fdcbf1c8a6199a4be64560d9
Izabelle bones <03062280093@senacrs.edu.br>
2025-03-27 11:28:55 -0300

    contador

---
+++ script.js
@@ -0,0 +1,14 @@
+//let numero = 0;
+
+const btnSoma = document.getElementById("botao");
+btnSoma.addEventListener('click', () => {
+    console.log('You visited the link');
+});
+
+
+/*
+btnSoma.addEventListener('click', () =>{
+    numero = numero +1;
+    document.getElementById('value').innerHTML(numero);
+});*/
+
